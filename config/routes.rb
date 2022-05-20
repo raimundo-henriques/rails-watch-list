44dda23205ce1e27510a4722fb90132a82a6fb1b
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-05-20 13:15:54 +0100

    nao sei

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, only: [ :show, :new, :create] do
+  resources :lists, only: [ :index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
