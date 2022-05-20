6cb77f9f56528114a7a1f807b0bf23a19af9147f
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-05-20 11:22:32 +0100

    Green rakes apart from destroy

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+  def new
+    set_list
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    set_list
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    set_list
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  # get 'bookmarks/new'
+  # get 'bookmarks/create'
+  # get 'bookmarks/delete'
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/new'
+  # get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
