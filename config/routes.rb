68127e438205991125a6b0c852a46e25860c53e0
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-05-20 13:05:18 +0100

    trying heroky

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   # get 'lists/new'
   # get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :new, :create] do
+  root to: 'lists#index'
+  resources :lists, only: [ :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
