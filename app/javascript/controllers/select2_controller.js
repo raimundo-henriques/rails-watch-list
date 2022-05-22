5eb7fc7fc8448eea494db0e9ba442b34cbf23898
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-05-22 16:24:12 +0100

    Style improvements (but...)

---
+++ app/javascript/controllers/select2_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus";
+import $ from "jquery";
+import "select2";
+import 'select2/dist/css/select2.min.css';
+
+export default class extends Controller {
+  connect() {
+     $(this.element).select2({
+      width: '100%'
+     });
+  }
+}
